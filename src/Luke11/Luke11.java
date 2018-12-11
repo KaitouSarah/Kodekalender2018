8121ff86cb54f73af4d606192629ca28fd2aba7e
Liv Sarah Ansnes <Liv.Sarah.Ansnes@INT.CCOE.CLOUD>
2018-12-11 17:33:28 +0100

    Added solution for Luke 11

---
+++ src/Luke11/Luke11.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Luke11 {
+    public static void main (String args[]) {
+        int y = 0;
+        int x = 0;
+        File file = new File("./inputLuke11.txt");
+        Scanner sc;
+
+        try {
+            sc = new Scanner(file).useDelimiter("");
+
+            while (sc.hasNext()) {
+                int number = sc.nextInt();
+                Character direction = sc.next().charAt(0);
+                if (direction == 'H') {
+                    x += number;
+                } else if (direction.equals('V')) {
+                    x -= number;
+                } else if (direction.equals('F')) {
+                    y += number;
+                } else if (direction.equals('B')) {
+                    y -= number;
+                } else {
+                    System.out.println("Something went horribly wrong!");
+                }
+             }
+
+            System.out.println("X = " + x + " and Y = " + y);
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
