796e784b81c3a6ca427e1438e008f34ad3ce32a5
Liv Sarah Ansnes <182021@stud.hvl.no>
2018-12-15 14:39:33 +0100

    Lagt til løsning for Luke15

--- src/Luke15/Luke15.java
+++ src/Luke15/Luke15.java
@@ -1,8 +1,46 @@
 package Luke15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Luke15 {
 
-    public static void main (String args[]) {
+    public static void main (String args[]) throws FileNotFoundException {
+        File file = new File("./src/Luke15/inputLuke15.txt");
+        Scanner sc = new Scanner(file);
 
+        System.out.println("Number of people with gullbursdag: " + findPplWithGullbursdag(sc));
+    }
+
+    private static int findPplWithGullbursdag(Scanner sc) {
+        int pplWithGullbursdag = 0;
+
+        while (sc.hasNextLine()) {
+            String[] line = sc.nextLine().split("\\.");
+            int yearOfBirth = Integer.parseInt(line[1]);
+
+            if (hasGullbursdag(yearOfBirth)) {
+                pplWithGullbursdag++;
+            }
+        }
+        return pplWithGullbursdag;
+    }
+
+    private static boolean hasGullbursdag(int yearOfBirth) {
+        boolean hasGullbursdag = true;
+        int yearCounter = yearOfBirth;
+        int ageounter = 0;
+        while (hasGullbursdag) {
+            yearCounter++;
+            ageounter++;
+
+            if (ageounter*ageounter == yearCounter) {
+                return hasGullbursdag;
+            } else if (ageounter*2 > yearCounter) {
+                hasGullbursdag = false;
+            }
+        }
+        return hasGullbursdag;
     }
 }
